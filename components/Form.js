9f2141649285f2892165ae9497848c40930ed9ec
geckoxys <edward.andre.23@gmail.com>
2022-11-02 17:50:51 -0400

    cambios finales 3

--- components/Form.js
+++ components/Form.js
@@ -5,8 +5,8 @@ const Form = ({handleChange, form, handleSubmit}) => {
         <form onSubmit={(e)=>e.preventDefault} autoComplete="off" className={styles.form}>
             <input type="text" name="nickname" placeholder="Nickname" onChange={handleChange} value={form.nickname} className={`${styles.form_el} ${styles.text}`} required/>
             <input type="password" name="password" placeholder="Password" onChange={handleChange} value={form.password} className={`${styles.form_el} ${styles.text}`} required/>
-            <input className={`${styles.button} ${ (!form.nickname|| form.password.length<8 || form.password.length>50 || form.nickname.length>20) ? styles.disabled : ""} ${styles.form_el} `}  type="button" value="Sign Up" onClick={handleSubmit}/>
-            <input className={`${styles.button} ${ (!form.nickname|| form.password.length<8 || form.password.length>50 || form.nickname.length>20) ? styles.disabled : ""} ${styles.form_el}`} type="button" value="Sign In" onClick={handleSubmit}/>
+            <input className={`${ (!form.nickname|| form.password.length<8 || form.password.length>50 || form.nickname.length>20) ? styles.disabled : styles.button} ${styles.form_el} `}  type="button" value="Sign Up" onClick={handleSubmit}/>
+            <input className={`${ (!form.nickname|| form.password.length<8 || form.password.length>50 || form.nickname.length>20) ? styles.disabled : styles.button} ${styles.form_el}`} type="button" value="Sign In" onClick={handleSubmit}/>
         </form>
     );
 }
